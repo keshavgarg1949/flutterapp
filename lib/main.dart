6bbc402d9cf63417024d6acc75b81512190e072f
Keshav Garg <garg22keshav75@gmail.com>
2021-05-14 21:35:33 +0530

    first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+
+  const Myapp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child : Center(     
+           child : Container(
+            child : Text("how do you do"),
+      
+    ),
+    ),
+  
+    ), 
+    );
+  }
+}
